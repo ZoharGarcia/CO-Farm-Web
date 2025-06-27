d3d7ef63eb30ae604ce6c5e294446ca1a78e2467
Zohar García <garciabrisa2007@gmail.com>
2025-06-26 22:50:12 -0600

    Update register.js

--- register.js
+++ register.js
@@ -7,12 +7,6 @@ document.getElementById('registerForm').addEventListener('submit', function (e) 
   const usuario = this[3].value.trim();
   const contrasena = this[4].value.trim();
 
-  if (!usuario || !contrasena || !nombre || !apellido || !finca) {
-    alert("Por favor, completa todos los campos.");
-    return;
-  }
-
-  // Revisión si ya existe el usuario
   const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
   const existe = usuarios.find(u => u.usuario === usuario);
 
@@ -21,9 +15,9 @@ document.getElementById('registerForm').addEventListener('submit', function (e) 
     return;
   }
 
-  // Guardar usuario
   usuarios.push({ usuario, contrasena, nombre, apellido, finca });
   localStorage.setItem('usuarios', JSON.stringify(usuarios));
   alert("¡Registro exitoso! Ahora puedes iniciar sesión.");
   window.location.href = "index.html";
 });
+
